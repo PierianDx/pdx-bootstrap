db8b249a36a94309fd16beab9d2ae67d1b6129a0
David Atchley <david.m.atchley@gmail.com>
2018-08-07 02:27:49 -0500

    docs: updated the documentation
    * now covers all components, toggle, flag dropdown and status dropdown
    * calls out that the typography is future feature and not part of the current release.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,24 @@
 var map = {
-"<": "&lt;",
-">": "&gt;",
-"&": "&amp;"
+  "<": "&lt;",
+  ">": "&gt;",
+  "&": "&amp;"
 };
 
 $('code[class*="language-"]').each((index, el) => {
   console.log(el);
-  $(el).html($(el).html().replace(/([<>\&])/g, (match, p1) => {
-    console.log('match:', p1, map[p1]);
-    return map[p1];
-  }))
+  $(el).html(
+    $(el)
+      .html()
+      .replace(/([<>\&])/g, (match, p1) => {
+        console.log("match:", p1, map[p1]);
+        return map[p1];
+      })
+  );
 });
 Prism.highlightAll(false, () => {
-  console.log('done highlighting')
-})
+  console.log("done highlighting");
+});
+
+$(".dropdown-flag").flagDropdown();
+$(".dropdown-status").statusDropdown();
+$(".toggle").toggleSwitch();
