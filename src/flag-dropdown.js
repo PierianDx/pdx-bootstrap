ce42e47a91073ea6e9e0d4bdec6fea49848a2b28
David Atchley <david.m.atchley@gmail.com>
2018-08-07 01:00:26 -0500

    fix(modules): updated modules for es6 and style changes.
    * modularized creating jquery plugins using es6 to clean up code.
    * updated .flag class to not require a nested span, and to use span rather than div.

--- src/flag-dropdown.js
+++ src/flag-dropdown.js
@@ -1,63 +1,51 @@
+// flag-dropdown.js
 import $ from "jquery";
 import pkg from "../package.json";
+import plugin from "./plugin";
 
-function FlagDropdown(element) {
-  this.$element = $(element);
-  var $selected = $(element).find("li.selected > a");
-  var $menu = this.$element.find(".dropdown-menu");
+class FlagDropdown {
+  constructor(element, options) {
+    const $element = $(element);
+    this.$element = $element;
+    var $menu = $element.find(".dropdown-menu");
+    var $selected = $menu.find("li.selected > a");
 
-  if ($selected.length) {
-    // Set any initial selected (last one wins)
-    this.setSelected($selected);
+    if ($selected.length) {
+      // Set any initial selected (last one wins)
+      this.setSelected($selected);
+      $element.trigger("pdx.flagdropdown.change", $element.data("selected"));
+    }
+    // Handle delegated clicks on dropdown-menu item links
+    $menu.on("click", "a", this.handleSelection.bind(this));
+  }
+
+  handleSelection(ev) {
+    var $target = $(ev.currentTarget);
+    this.toggleItem($target);
+    this.setSelected($target);
     this.$element.trigger(
       "pdx.flagdropdown.change",
       this.$element.data("selected")
     );
   }
-  // Handle delegated clicks on dropdown-menu item links
-  $menu.on("click", "a", this.handleSelection.bind(this));
+
+  toggleItem($item) {
+    $item.closest("li").toggleClass("selected");
+  }
+
+  setSelected($selected) {
+    var selected = this.$element
+      .find(".dropdown-menu > li.selected > a")
+      .toArray()
+      .map(function(el) {
+        return el.getAttribute("id");
+      });
+    this.$element.data("selected", selected);
+  }
 }
 
 FlagDropdown.VERSION = pkg.version;
 
-FlagDropdown.prototype.handleSelection = function(ev) {
-  var $target = $(ev.currentTarget);
-  this.toggleItem($target);
-  this.setSelected($target);
-  this.$element.trigger(
-    "pdx.flagdropdown.change",
-    this.$element.data("selected")
-  );
-};
+FlagDropdown.DEFAULTS = {};
 
-FlagDropdown.prototype.toggleItem = function($item) {
-  $item.closest("li").toggleClass("selected");
-};
-
-FlagDropdown.prototype.setSelected = function($selected) {
-  var selected = this.$element
-    .find(".dropdown-menu > li.selected > a")
-    .toArray()
-    .map(function(el) {
-      return el.getAttribute("id");
-    });
-  this.$element.data("selected", selected);
-};
-
-// JQuery plugin
-$.fn.flagDropdown = function(option) {
-  return this.each(function() {
-    var $this = $(this);
-    var data = $this.data("pdx.flagdropdown");
-
-    if (!data) $this.data("pdx.flagdropdown", (data = new FlagDropdown(this)));
-    if (typeof option == "string") data[option].call($this);
-  });
-};
-
-// JQuery Plugin administrative stuff
-$.fn.flagDropdown.Constructor = FlagDropdown;
-$.fn.flagDropdown.noConflict = function() {
-  $.fn.flagDropdown = old;
-  return this;
-};
+plugin("flagDropdown", FlagDropdown);
--- src/toggle-switch.js
+++ src/toggle-switch.js
@@ -1,48 +1,97 @@
+// import $ from "jquery";
+// import pkg from "../package.json";
+
+// function ToggleSwitch(element) {
+//   this._$input = $(element).find("input.toggle-switch");
+//   // console.log('> input:', this._$input);
+//   if ($(element).hasClass("checked")) {
+//     // initialize checked state
+//     $(element).data("checked", true);
+//     this._$input.prop("checked", true);
+//   }
+//   $(element).on("click", this.toggle.bind(this));
+// }
+
+// ToggleSwitch.VERSION = pkg.version;
+
+// ToggleSwitch.prototype.toggle = function(ev) {
+//   var element = ev.currentTarget;
+//   var $element = $(element);
+//   if ($element.is(".disabled, :disabled")) return;
+
+//   // Set checked state
+//   var state = !this._$input.prop("checked");
+//   this._$input.prop("checked", state);
+//   $element.data("checked", state);
+//   $element.toggleClass("checked", state);
+
+//   $element.trigger("pdx.toggle.change", state);
+//   ev.preventDefault();
+// };
+
+// // JQuery plugin
+// $.fn.toggleSwitch = function(option) {
+//   return this.each(function() {
+//     var $this = $(this);
+//     var data = $this.data("pdx.toggle");
+
+//     if (!data) $this.data("pdx.toggle", (data = new ToggleSwitch(this)));
+//     if (typeof option == "string") data[option].call($this);
+//   });
+// };
+
+// // JQuery Plugin administrative stuff
+// $.fn.toggleSwitch.Constructor = ToggleSwitch;
+// $.fn.toggleSwitch.noConflict = function() {
+//   $.fn.toggleSwitch = old;
+//   return this;
+// };
+
+// status-dropdown.js
 import $ from "jquery";
 import pkg from "../package.json";
+import plugin from "./plugin";
 
-function ToggleSwitch(element) {
-  this._$input = $(element).find("input.toggle-switch");
-  // console.log('> input:', this._$input);
-  if ($(element).hasClass("checked")) {
-    // initialize checked state
-    $(element).data("checked", true);
-    this._$input.prop("checked", true);
+class ToggleSwitch {
+  constructor(element, options) {
+    const $element = $(element);
+    this.$element = $element;
+    this._$input = $element.find("input.toggle-switch");
+    // console.log('> input:', this._$input);
+    if ($element.hasClass("checked")) {
+      // initialize checked state
+      $element.data("checked", true);
+      this._$input.prop("checked", true);
+    }
+    $element.on("click", this.toggle.bind(this));
   }
-  $(element).on("click", this.toggle.bind(this));
+
+  handleSelection(ev) {
+    var element = ev.currentTarget;
+    var $element = $(element);
+    if ($element.is(".disabled, :disabled")) return;
+
+    // Set checked state
+    var state = !this._$input.prop("checked");
+    this._$input.prop("checked", state);
+    $element.data("checked", state);
+    $element.toggleClass("checked", state);
+
+    $element.trigger("pdx.toggle.change", state);
+    ev.preventDefault();
+  }
+
+  setSelected($selected) {
+    this.$element.data("selected", $selected.attr("id"));
+    // Set the toggle to reflect the selected item
+    this.$element.find("[data-toggle]").get(0).className = $selected
+      .find("i")
+      .get(0).className;
+  }
 }
 
 ToggleSwitch.VERSION = pkg.version;
 
-ToggleSwitch.prototype.toggle = function(ev) {
-  var element = ev.currentTarget;
-  var $element = $(element);
-  if ($element.is(".disabled, :disabled")) return;
+ToggleSwitch.DEFAULTS = {};
 
-  // Set checked state
-  var state = !this._$input.prop("checked");
-  this._$input.prop("checked", state);
-  $element.data("checked", state);
-  $element.toggleClass("checked", state);
-
-  $element.trigger("pdx.toggle.change", state);
-  ev.preventDefault();
-};
-
-// JQuery plugin
-$.fn.toggleSwitch = function(option) {
-  return this.each(function() {
-    var $this = $(this);
-    var data = $this.data("pdx.toggle");
-
-    if (!data) $this.data("pdx.toggle", (data = new ToggleSwitch(this)));
-    if (typeof option == "string") data[option].call($this);
-  });
-};
-
-// JQuery Plugin administrative stuff
-$.fn.toggleSwitch.Constructor = ToggleSwitch;
-$.fn.toggleSwitch.noConflict = function() {
-  $.fn.toggleSwitch = old;
-  return this;
-};
+plugin("toggleSwitch", ToggleSwitch, "toggle");
